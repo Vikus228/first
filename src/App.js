6bfbe828ebed912c9ef7fb3af9cb70c4b9429c9c
Vikus228 <vika.kolesnikovs.1990@gmail.com>
2024-10-12 23:41:34 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
+
 
 function App() {
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Работа", 3],
+    ["Перекусы", 2],
+    ["Общение", 5],
+    ["Учёба", 5],
+    ["Сон", 9],
+  ];
+
+  const options = {
+    title: "Мой день",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"600px"}
+    />
   );
 }
 
